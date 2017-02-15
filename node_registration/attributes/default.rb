8d8819f2ba7261e1931e9abaa150a98ee04faa47
damith <damith@ravindur.local>
2017-02-15 15:39:25 +0530

    dev chef server updated

--- node_registration/attributes/default.rb
+++ node_registration/attributes/default.rb
@@ -1,3 +1,3 @@
 default["node_registration"]["environment"] = "dev"
 default["node_registration"]["chef_server_url"] = "https://52.214.7.179/"
-default["node_registration"]["role"] = "appserver"
+default["node_registration"]["role"] = "app-server"
