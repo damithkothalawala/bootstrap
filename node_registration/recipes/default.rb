820094869e6b54e0a87af04398e96e7b829f8f09
Damith Rushika Kothalawala <admin@drklk.org>
2017-02-15 22:54:33 +0530

    Update default.rb

--- node_registration/recipes/default.rb
+++ node_registration/recipes/default.rb
@@ -31,10 +31,10 @@ bash "s3_downloads" do
   cp -f /tmp/chef-validator.pem /etc/chef/validation.pem | true
   cp -f /tmp/data_bag_secret /etc/chef/encrypted_data_bag_secret | true
   # Bootstrap Chef Client
-  mkdir -p /var/chef/cache /var/chef/cookbooks/chef-client /var/chef/cookbooks/cron /var/chef/cookbooks/logrotate | true
-  wget -qO- https://github.com/opscode-cookbooks/cron/archive/v1.2.6.tar.gz | tar xvzC /var/chef/cookbooks/cron --strip-components=1 | true
-  wget -qO- https://github.com/opscode-cookbooks/logrotate/archive/v1.3.0.tar.gz | tar xvzC /var/chef/cookbooks/logrotate --strip-components=1 | true
-  wget -qO- https://github.com/opscode-cookbooks/chef-client/archive/v3.7.0.tar.gz | tar xvzC /var/chef/cookbooks/chef-client --strip-components=1 | true
+  mkdir -p /var/chef/cache /var/chef/cookbooks/chef-client /var/chef/cookbooks/cron /var/chef/cookbooks/logrotate 
+  wget -qO- https://github.com/opscode-cookbooks/cron/archive/v1.2.6.tar.gz | tar xvzC /var/chef/cookbooks/cron --strip-components=1 
+  wget -qO- https://github.com/opscode-cookbooks/logrotate/archive/v1.3.0.tar.gz | tar xvzC /var/chef/cookbooks/logrotate --strip-components=1 
+  wget -qO- https://github.com/opscode-cookbooks/chef-client/archive/v3.7.0.tar.gz | tar xvzC /var/chef/cookbooks/chef-client --strip-components=1 
   #/usr/bin/chef-solo -j /etc/chef/chef.json > /tmp/chef_solo.log  
   # Execute roles
   #/usr/bin/chef-client  -j /etc/chef/roles.json --once > /tmp/chef_client.log 2>&1 
