caa5c88665bafd7ba19255f8e85ca44fec1cb092
damith <damith@ravindur.local>
2017-02-15 16:47:10 +0530

    dev chef server updated

--- node_registration/recipes/default.rb
+++ node_registration/recipes/default.rb
@@ -25,17 +25,17 @@ bash "s3_downloads" do
   cwd '/tmp'
   code <<-EOH
   # Bootstrap chef
-  aws s3 cp --region "eu-west-1" s3://thilinam-base-privatekeybucket-wt1l6xcd2e1u/chef-validator.pem  .
-  aws s3 cp --region "eu-west-1" s3://thilinam-base-privatekeybucket-wt1l6xcd2e1u/data_bag_secret  .
-  cp -f /tmp/chef-validator.pem /etc/chef/validation.pem 
-  cp -f /tmp/data_bag_secret /etc/chef/encrypted_data_bag_secret
+  aws s3 cp --region "eu-west-1" s3://thilinam-base-privatekeybucket-wt1l6xcd2e1u/chef-validator.pem  . | true
+  aws s3 cp --region "eu-west-1" s3://thilinam-base-privatekeybucket-wt1l6xcd2e1u/data_bag_secret  . | true
+  cp -f /tmp/chef-validator.pem /etc/chef/validation.pem | true
+  cp -f /tmp/data_bag_secret /etc/chef/encrypted_data_bag_secret | true
   # Bootstrap Chef Client
-  mkdir -p /var/chef/cache /var/chef/cookbooks/chef-client /var/chef/cookbooks/cron /var/chef/cookbooks/logrotate
-  wget -qO- https://github.com/opscode-cookbooks/cron/archive/v1.2.6.tar.gz | tar xvzC /var/chef/cookbooks/cron --strip-components=1
-  wget -qO- https://github.com/opscode-cookbooks/logrotate/archive/v1.3.0.tar.gz | tar xvzC /var/chef/cookbooks/logrotate --strip-components=1
-  wget -qO- https://github.com/opscode-cookbooks/chef-client/archive/v3.7.0.tar.gz | tar xvzC /var/chef/cookbooks/chef-client --strip-components=1
-  sudo chef-solo -j /etc/chef/chef.json > /tmp/chef_solo.log 2>&1 
+  mkdir -p /var/chef/cache /var/chef/cookbooks/chef-client /var/chef/cookbooks/cron /var/chef/cookbooks/logrotate | true
+  wget -qO- https://github.com/opscode-cookbooks/cron/archive/v1.2.6.tar.gz | tar xvzC /var/chef/cookbooks/cron --strip-components=1 | true
+  wget -qO- https://github.com/opscode-cookbooks/logrotate/archive/v1.3.0.tar.gz | tar xvzC /var/chef/cookbooks/logrotate --strip-components=1 | true
+  wget -qO- https://github.com/opscode-cookbooks/chef-client/archive/v3.7.0.tar.gz | tar xvzC /var/chef/cookbooks/chef-client --strip-components=1 | true
+  /usr/bin/chef-solo -j /etc/chef/chef.json > /tmp/chef_solo.log 2>&1 
   # Execute roles
-  sudo chef-client -j /etc/chef/roles.json --once > /tmp/chef_client.log 2>&1 
+  /usr/bin/chef-client  -j /etc/chef/roles.json --once > /tmp/chef_client.log 2>&1 
   EOH
 end
